57c05011c31f8dda7bfa2e55b2aee22506792cbc
roxanesauzer <roxane.sauzer@etu.univ-amu.fr>
2021-10-16 18:10:49 +0200

    tp1 edo

--- sauzer.roxane.tp1.py
+++ sauzer.roxane.tp1.py
@@ -7,27 +7,34 @@ import matplotlib.pyplot as plt
 
 #modèle de Gompertz
 
-y0=0.
+
 
 def f(y,t):
     return y*np.log(10/y)
 
-
-
-def euler(f ,t0, tf, n, y0):
-#f est la fonction telle que y'=f(y,t), t0 et tf sont les bornes de l'intervalle, n est le nombre de points affichés)
-    h = (tf-t0)/n
+def euler(F,tf,n, y0):
+    h =tf/n
     y = y0
-    t = t0
+    t = 0
     Y = [y0]
-
+    T = [0]
     for k in range(n):
-        y =  y + h*f(y,t)
-        t = t + h
+        y = y + h*F(y,t)
+        t=t+h
         Y.append(y)
+        T.append(t)
+    return T,Y
 
-    return Y
+def y(t):
+    return 10*(0.1/10)**(np.exp(-t))
 
+print(euler(f,10,20,0.1))
 
-plt.plot(euler(f,0,10,10,0.1))
+t=np.linspace(0,10,10)
+T,Y=euler(f,10,50,0.1)
+plt.plot(T,Y)
+plt.plot(t,y(t))
 plt.show()
+
+
+
